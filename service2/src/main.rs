7fcf2cde51c4727c146e10a1b30e705c58e78891
David Lakos <lakosd@lakosd.net>
2024-10-09 21:49:40 +0300

    Change service2 implementation to Rust

---
+++ service2/src/main.rs
@@ -0,0 +1,85 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use serde_json::json;
+use std::process::{Command, Stdio};
+use std::str;
+
+async fn get_system_info() -> impl Responder {
+    let df = Command::new("df")
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+    let awk = Command::new("awk")
+        .arg("$6 == \"/\" {print $4}")
+        .stdin(Stdio::from(df.stdout.unwrap()))
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+    let df_output = awk.wait_with_output().unwrap();
+    let df_result = str::from_utf8(&df_output.stdout).unwrap().trim();
+    
+    let uptime = Command::new("uptime")
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+    let sed = Command::new("sed")
+        .arg("s/^ [^ ]* up \\([^,]*,[^,]*\\),.*/\\1/")
+        .stdin(Stdio::from(uptime.stdout.unwrap()))
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+    let uptime_output = sed.wait_with_output().unwrap();
+    let uptime_result = str::from_utf8(&uptime_output.stdout).unwrap().trim();
+
+    let hostname = Command::new("hostname")
+        .arg("-i")
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+    let hostname_output = hostname.wait_with_output().unwrap();
+    let hostname_result = str::from_utf8(&hostname_output.stdout).unwrap().trim();
+
+    let ps = Command::new("ps")
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+    let ps_output = ps.wait_with_output().unwrap();
+    let ps_result = str::from_utf8(&ps_output.stdout).unwrap().trim();
+
+    // Parse ps output
+    let ps_lines: Vec<String> = ps_result
+        .split('\n')
+        .map(|line| line.trim().to_string())
+        .skip(1)
+        .collect();
+
+    // let parsed_ps_data: Vec<Option<serde_json::Value>> = ps_lines.into_iter()
+    //     .map(|line| {
+    //         if line.starts_with("PID") {
+    //             None
+    //         } else {
+    //             serde_json::from_str(&line).ok()
+    //         }
+    //     })
+    //     .collect();
+
+    let system_info = json!({
+        "ip": hostname_result,
+        "processes": ps_lines,
+        "diskSpace": df_result,
+        "uptime": uptime_result,
+    });
+
+    HttpResponse::Ok().json(system_info)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(get_system_info))
+    })
+    .bind(("127.0.0.1", 8198))?
+    .run()
+    .await
+}
+
